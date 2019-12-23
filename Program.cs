b2f14abe3ef530ea7073eb8b383c5d152bcf6dee
Alexy102 <59171672+Alexy102@users.noreply.github.com>
2019-12-23 20:47:34 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение x:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y;
+            if (x < -1)
+            {
+                y = 1 / x;
+            }
+            if (x >= -1 && x <= 4)
+            {
+                y = x * x;
+            }
+            else
+            {
+                y = 4;
+            }
+            Console.WriteLine("Значение функции равно = " + y);
+        }
+    }
+}
